919b4f8c38e9758a97b01716851c7eb5b5e4d8ad
Helmik Escamilla <hescamilla@itexico.net>
2017-04-12 10:57:28 -0500

    insert Roles and set acl

--- server/boot/database/index.js
+++ server/boot/database/index.js
@@ -4,7 +4,10 @@
  * Created by developeri on 4/10/17.
  */
 module.exports = function(app) {
+  var RoleMapping = app.models.RoleMapping;
   var data = [];
+  // Get data to save
+  data.push(require('./Role.json'));
   data.push(require('./catStatus.json'));
   data.push(require('./user.json'));
   var saveData = function(model, data) {
@@ -13,13 +16,10 @@ module.exports = function(app) {
       // Count the data in table
       model.count().then(function(count) {
         if (count <= 0) {
-          // resolve(model.create(data.data));
-          //data.data.map(function(obj){
-
-          //});
+          // Fill table
           resolve(model.create(data));
         } else {
-          rejected('Table has data already.');
+          resolve('Table has data already.');
         }
       }).catch(function(error) {
         rejected(error);
@@ -27,11 +27,32 @@ module.exports = function(app) {
     });
   };
   var models = app.models;
-  data.map(function(d) {
-    saveData(models[d.model], d.data).then(function(response) {
-      console.log('Data inserted', response);
-    }).catch(function(error) {
-      console.log(error);
+  var promises = [];
+  data.map(function(model) {
+    promises.push(saveData(models[model.model], model.data));
+  });
+  Promise.all(promises).then(function(response) {
+    console.log('Data inserted', response);
+    // Relationships between customers and roles
+    var relationships = [
+      {role: 'Admin', username: 'admin'},
+    ];
+    relationships.forEach(function(relationship) {
+      var queryRole = {where: {name: relationship.role}};
+      console.log(queryRole);
+      models.Role.find(queryRole, function(err, role) {
+        if (err) throw err;
+        var queryName = {where: {username: relationship.username}};
+        models.User.find(queryName, function(err, user) {
+          if (err) throw err;
+          role.pop().principals.create({
+            principalType: RoleMapping.USER,
+            principalId: user.pop().id,
+          });
+        });
+      });
     });
+  }).catch(function(error) {
+    console.log(error);
   });
 };
